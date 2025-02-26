cb34c5a699234a61f8659a3804cab64858d3533b
Alex Moeller <alexmoe@iastate.edu>
2025-02-26 17:07:43 -0600

    app.py change, index html updates

--- Chapter-7/photogallery/app.py
+++ Chapter-7/photogallery/app.py
@@ -1,27 +1,3 @@
-'''
-MIT License
-
-Copyright (c) 2019 Arshdeep Bahga and Vijay Madisetti
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-'''
-
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask import render_template, redirect
@@ -33,20 +9,21 @@ from boto3.dynamodb.conditions import Key, Attr
 import exifread
 import json
 
-app = Flask(__name__, static_url_path="")
+app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
 
 UPLOAD_FOLDER = os.path.join(app.root_path,'media')
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-AWS_ACCESS_KEY="<enter>"
-AWS_SECRET_KEY="<enter>+0vqOmhI3ObEtIvQ+jmAkh/"
-REGION="us-east-1"
-BUCKET_NAME="<enter>"
+AWS_ACCESS_KEY="AKIATNVEVPSKVP4QH2LN"
+AWS_SECRET_KEY="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"
+REGION="us-east-2"
+BUCKET_NAME="team9-photostorage-bucket-project2"
 
 dynamodb = boto3.resource('dynamodb', aws_access_key_id=AWS_ACCESS_KEY,
                             aws_secret_access_key=AWS_SECRET_KEY,
                             region_name=REGION)
 
-table = dynamodb.Table('PhotoGallery')
+# Updated table name to 'photo_gallery'
+table = dynamodb.Table('photo_gallery')
 
 
 def allowed_file(filename):
@@ -123,9 +100,10 @@ def add_photo():
                         fromtimestamp(ts).\
                         strftime('%Y-%m-%d %H:%M:%S')
 
+            # Updated partition key to 'photo_id'
             table.put_item(
             Item={
-                    "PhotoID": str(int(ts*1000)),
+                    "photo_id": str(int(ts*1000)),  # Updated to 'photo_id'
                     "CreationTime": timestamp,
                     "Title": title,
                     "Description": description,
@@ -141,8 +119,9 @@ def add_photo():
 
 @app.route('/<int:photoID>', methods=['GET'])
 def view_photo(photoID):
+    # Updated partition key to 'photo_id'
     response = table.scan(
-        FilterExpression=Attr('PhotoID').eq(str(photoID))
+        FilterExpression=Attr('photo_id').eq(str(photoID))  # Updated to 'photo_id'
     )
 
     items = response['Items']
